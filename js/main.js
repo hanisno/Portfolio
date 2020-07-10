2e66081fc0a086590bec8148598da74529790c6d
hanis <hanisno@outlook.com>
2020-07-10 19:55:15 +0800

    Add full nav features and initialize link hover fx

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$("#menu-toggle").click(function () {
+  $("header").toggleClass("open");
+});
