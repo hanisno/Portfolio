800c22e2d85188e1fb771f106c95a33f14c12e3b
hanis <hanisno@outlook.com>
2020-07-22 19:13:52 +0800

    Add image slides on work detail page

--- js/main.js
+++ js/main.js
@@ -3,28 +3,52 @@ $("#menu-toggle").click(function () {
 });
 
 /*Credit to lovromar on jsfiddle for the parallax effect code*/
-var currentX = "";
-var currentY = "";
-var movementConstant = 0.002;
-$("main").mousemove(function (e) {
-  if (currentX == "") currentX = e.clientX;
-  var xdiff = e.clientX - currentX;
-  currentX = e.clientX;
-  if (currentY == "") currentY = e.clientY;
-  var ydiff = e.pageY - currentY;
-  currentY = e.pageY;
-  $(".parallaxfx img").each(function (i, el) {
-    var movement = (i + 1) * (xdiff * movementConstant);
-    var movementy = (i + 1) * (ydiff * movementConstant);
-    var newX = $(el).position().left + movement;
-    var newY = $(el).position().top + movementy;
-    $(el).css("left", newX + "px");
-    $(el).css("top", newY + "px");
-  });
-});
-
-/*Work detail image slides*/
 $(document).ready(function () {
+  var currentX = "";
+  var currentY = "";
+  var movementConstant = 0.002;
+  $("main").mousemove(function (e) {
+    if (currentX == "") currentX = e.clientX;
+    var xdiff = e.clientX - currentX;
+    currentX = e.clientX;
+    if (currentY == "") currentY = e.clientY;
+    var ydiff = e.pageY - currentY;
+    currentY = e.pageY;
+    $(".parallaxfx img").each(function (i, el) {
+      var movement = (i + 1) * (xdiff * movementConstant);
+      var movementy = (i + 1) * (ydiff * movementConstant);
+      var newX = $(el).position().left + movement;
+      var newY = $(el).position().top + movementy;
+      $(el).css("left", newX + "px");
+      $(el).css("top", newY + "px");
+    });
+  });
+
+  /*$(document).ready(function () {
+    var currentX = "";
+    var currentY = "";
+    var movementConstant = 0.002;
+    $("main").mousemove(function (e) {
+      if (currentX == "") currentX = e.clientX;
+      var xdiff = e.clientX - currentX;
+      currentX = e.clientX;
+      if (currentY == "") currentY = e.clientY;
+      var ydiff = e.pageY - currentY;
+      currentY = e.pageY;
+      $(".parallaxfx img").each(function (i, el) {
+        setTimeout(function () {
+          var movement = (i + 1) * (xdiff * movementConstant);
+          var movementy = (i + 1) * (ydiff * movementConstant);
+          var newX = $(el).position().left + movement;
+          var newY = $(el).position().top + movementy;
+          $(el).css("left", newX + "px");
+          $(el).css("top", newY + "px");
+        }, 1000);
+        clearTimeout();
+      });
+    });*/
+  /*Work detail image slides*/
+
   $(".next").on("click", function () {
     var activeImg = $(".active");
     var nextImg = activeImg.next();
